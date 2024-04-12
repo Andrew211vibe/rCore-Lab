44cd3d05bd2bc33e2b8208fc18e29580668cb335
Andrew211vibe <1207368051@qq.com>
2024-04-12 14:29:19 +0800

    update syscall

---
+++ os/src/syscall/process.rs
@@ -0,0 +1,7 @@
+use crate::batch::run_next_app;
+
+/// task exits and submit an exit code
+pub fn sys_exit(xstate: i32) -> ! {
+    println!("[kernel] Application exited with code {}", xstate);
+    run_next_app();
+}
