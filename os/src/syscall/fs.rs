48ffc30ea6339954c162a7c9b1473b433fd9f5d2
Andrew211vibe <1207368051@qq.com>
2024-04-15 16:08:19 +0800

    update

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -6,18 +6,10 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     trace!("kernel: sys_write");
     match fd {
         FD_STDOUT => {
-            if (((buf as usize) >= USER_STACK.get_sp() - USER_STACK_SIZE) // 在用户栈地址范围内
-                    && ((buf as usize) + len <= USER_STACK.get_sp()))
-                || (((buf as usize) + len <= APP_SIZE_LIMIT + APP_BASE_ADDRESS) // 在应用程序大小范围内
-                    && ((buf as usize) >= APP_BASE_ADDRESS))
-            {
-                let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-                let str = core::str::from_utf8(slice).unwrap();
-                print!("{}", str);
-                len as isize
-            } else {
-                -1 as isize
-            }
+            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
+            let str = core::str::from_utf8(slice).unwrap();
+            print!("{}", str);
+            len as isize
         }
         _ => {
             panic!("Unsupported fd in sys_write!");
