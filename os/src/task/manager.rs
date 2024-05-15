c45c235fd547949537958a0e9f5ec33261402561
Andrew211vibe <1207368051@qq.com>
2024-05-15 10:14:15 +0800

    fix stride algo

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -1,4 +1,5 @@
 use super::TaskControlBlock;
+use crate::config::BIG_STRIDE;
 use crate::sync::UPSafeCell;
 use alloc::collections::VecDeque;
 use alloc::sync::Arc;
@@ -21,16 +22,35 @@ impl TaskManager {
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        let mut min_stride = 0;
-        let mut index = 0;
-        for idx in 0..self.ready_queue.len() {
-            let cur_stride = self.ready_queue[idx].inner_exclusive_access().stride;
-            if cur_stride < min_stride {
-                min_stride = cur_stride;
-                index = idx;
-            }
-        }
-        self.ready_queue.remove(index)
+        // let mut min_stride = 0;
+        // let mut index = 0;
+        // for idx in 0..self.ready_queue.len() {
+        //     let cur_stride = self.ready_queue[idx].inner_exclusive_access().stride;
+        //     if min_stride == 0 && idx == 0 {
+        //         min_stride = cur_stride;
+        //         continue;
+        //     }
+        //     println!("    {}-{}-cur:{}-min:{}", index, self.ready_queue[idx].getpid(), cur_stride, min_stride);
+        //     if cur_stride < min_stride {
+        //         min_stride = cur_stride;
+        //         index = idx;
+        //     }
+        // }
+        self.ready_queue.make_contiguous().sort_by_key(|item| {
+            let cur_stride = item.inner_exclusive_access().stride;
+            cur_stride
+        });
+        let min_stride = self.ready_queue.front()?.inner_exclusive_access().stride;
+        self.ready_queue.iter_mut()
+            .for_each(|item| {
+            let mut inner = item.inner_exclusive_access();
+            // println!("    pid:{}-stride:{}-pass:{}", item.getpid(), inner.stride, inner.pass);
+            inner.stride -= min_stride;
+        });
+        // println!("pop task: pid-{} stride-{}", 
+        //     self.ready_queue.front()?.getpid(), 
+        //     self.ready_queue.front()?.inner_exclusive_access().stride);
+        self.ready_queue.pop_front()
     }
 }
 
